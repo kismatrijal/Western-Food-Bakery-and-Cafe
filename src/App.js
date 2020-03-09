4d78371599333e936f1f13f2524db61204ded450
kismatrijal <kismatrijal@gmail.com>
2020-03-09 10:39:30 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render (){
+    return <h1> hello from western bakery</h1>;
+  }
+}
+
+export default App;
