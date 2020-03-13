33ef43d0bb56f8b9f1cd4a1e2ce052eb7e985ab4
kismatrijal <kismatrijal@gmail.com>
2020-03-14 02:05:43 +0545

    products details --single product

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -7,7 +7,7 @@ import {ProductConsumer} from '../context';
 export default function SingleProductPage(){
     return(
         <>
-           <Hero img ={SingleProductImg} title="single prodcut"/>
+           <Hero img ={SingleProductImg} title="single producut"/>
            <ProductConsumer>
                {value=> {
                    const {singleProduct, addToCart, loading}= value;
